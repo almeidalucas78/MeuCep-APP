b04f9c9a665522729c49c311932b018db91f6c3c
Andrew Ribeiro <andrew.ribeiro@etec.sp.gov.br>
2019-07-23 22:03:42 -0300

    Implementado estrutura de listagem e classe de domínio Endereço

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Endereco from 'src/app/domain/Endereco'
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,18 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+
+  enderecos:Array<Object> = []
+
+  ionViewDidEnter() {
+    let endereco1 = new Endereco()
+    endereco1.rua = 'Rua Cambará'
+    endereco1.bairro = 'Jardim Miray'
+    endereco1.cidade = 'Itaquaquecetuba'
+    endereco1.estado = 'SP'
+    endereco1.numero = '866'
+
+    this.enderecos.push(endereco1)
+  }
 
 }
